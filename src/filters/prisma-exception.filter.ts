5a646265ac90771f50b736e39e7b922eab8dabb3
noureldin <noureldin2662002@gmail.com>
2026-04-17 22:57:40 +0200

    finished exception handling

--- src/filters/prisma-exception.filter.ts
+++ src/filters/prisma-exception.filter.ts
@@ -10,16 +10,58 @@ import { Prisma } from '@prisma/client';
 export class PrismaExceptionFilter implements ExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
-
+    const request = host.switchToHttp().getRequest();
+console.log('Prisma filter hit');
+    // Unique constraint
     if (exception.code === 'P2002') {
+      const fields =
+        (exception.meta?.driverAdapterError as any)?.cause?.constraint
+          ?.fields ?? [];
+      const message =
+        fields.length > 1
+          ? `${fields.join(' and ')} already exist`
+          : `${fields[0]} already exists`;
       return response.status(409).json({
         statusCode: 409,
-        message: 'Unique constraint failed',
+        error: 'Conflict',
+        short_error_message: (exception.meta?.driverAdapterError as any)?.cause
+          ?.originalMessage,
+        message:message,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        request_type: request.method,
+        // full_error: exception,
+      });
+    }
+
+    // Record not found
+    if (exception.code === 'P2025') {
+      const fields =
+        (exception.meta?.driverAdapterError as any)?.cause?.constraint
+          ?.fields ?? [];
+      const message =
+        fields.length > 1
+          ? `${fields.join(' and ')} Are Not Found`
+          : `${fields[0]} Is Not Found`;
+      return response.status(404).json({
+        statusCode: 404,
+        error: 'Not Found',
+        message: (exception.meta?.driverAdapterError as any)?.cause
+          ?.originalMessage,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        request_type: request.method,
       });
     }
 
     return response.status(500).json({
-      message: 'Internal server error',
+      statusCode: 500,
+      error: 'Prisma Error',
+      message: (exception.meta?.driverAdapterError as any)?.cause
+        ?.originalMessage,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      request_type: request.method,
     });
   }
 }
